48a119a5412b3a1d015eb23aa66268910e2e2c1f
Patrick Lyons <X00158260@mytudublin.ie>
2024-02-15 15:26:40 +0400

    added the requirements.txt to store the necessary packages needed

--- prediction-mechanism/prediction_mechanism.py
+++ prediction-mechanism/prediction_mechanism.py
@@ -1,3 +1,11 @@
 #This file will use the daily sentiment value
 #as the exogeneous variable in the ARIMAX model
 #I will add the code for this
+
+data = {
+    'time': ['2022-01-01', '2022-01-02', '2022-01-03', '2022-01-04', '2022-01-05'],
+    'bitcoin_adj_close': [30000, 31000, 32000, 33000, 34000],
+    'sentiment': [0.2, 0.5, -0.1, 0.3, -0.2]
+}
+
+df = ''
